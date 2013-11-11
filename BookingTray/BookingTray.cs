75797295ee004ca35affeceb8d15ea412d8866da
Michael J <michael.git@shadowfox.net>
2013-11-11 10:03:04 +0000

    Initial commit - a basic static tray icon

---
+++ BookingTray/BookingTray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookingTray
+{
+    public partial class BookingTray : Form
+    {
+        private static ContextMenu trayMenu;
+        private static NotifyIcon trayIcon;
+
+        public BookingTray()
+        {
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Booking Tray";
+            trayIcon.Icon = TrayIcon.GetIcon("5");
+            trayIcon.Visible = true;
+            trayIcon.ContextMenu = trayMenu;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            // We don't want the application itself to be visible anywhere
+            Visible = false;
+            ShowInTaskbar = false;
+
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="isDisposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                trayIcon.Dispose();
+
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+
+            base.Dispose(isDisposing);
+        }
+    }
+}
---
+++ BookingTray/TrayIcon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+namespace BookingTray
+{
+    /// <summary>
+    /// Used to dynamically generate bitmap images for the system tray
+    /// </summary>
+    static class TrayIcon
+    {
+        /// <summary>
+        /// The width of the bitmap image
+        /// </summary>
+        private const int imageWidth = 40;
+
+        /// <summary>
+        /// The height of the bitmap image
+        /// </summary>
+        private const int imageHeight = 40;
+
+        /// <summary>
+        /// Get an icon
+        /// </summary>
+        /// <param name="iconText">The text to show in the icon - will be a one or two digit number</param>
+        /// <returns>A dynamically created Icon instance</returns>
+        public static Icon GetIcon(string iconText)
+        {
+            Bitmap bitmap = new Bitmap(imageWidth, imageHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            Rectangle rectangle = new Rectangle(0, 0, imageWidth, imageHeight);
+
+            int textOffset = 0;
+            if (iconText.Length < 2)
+                textOffset = 5;
+
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.FillRectangle(System.Drawing.Brushes.Yellow, 0, 0, imageWidth, imageHeight);
+            graphics.DrawString(iconText, new Font("Arial", 26, FontStyle.Bold), System.Drawing.Brushes.Black, new PointF(textOffset, 0));
+
+            IntPtr hBitmap = bitmap.GetHicon();
+            return Icon.FromHandle(hBitmap);
+        }
+    }
+}
