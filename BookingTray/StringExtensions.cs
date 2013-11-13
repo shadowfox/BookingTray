d0cea1cc6e6ed516ae8031091ecc4695f774f0ed
Michael J <michael.git@shadowfox.net>
2013-11-13 05:08:17 +0000

    Add a format string method

---
+++ BookingTray/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingTray
+{
+    public static class StringExtensions
+    {
+        public static string FormatWith(this string formatString, params object[] args)
+        {
+            return string.Format(formatString, args);
+        }
+    }
+}
